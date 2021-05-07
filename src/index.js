f324dcae8c7c65797213a51a1261c82c635a1a91
saurabh021120 <saurabhratnaparkhi11@gmail.com>
2021-05-07 19:40:01 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from 'react-dom';
 import App from './App'
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.render(
