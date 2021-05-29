0425e5970f98a7ea59147875a284ddab3f5caa69
Rudraksh <rudrakshsattabhayya@gmail.com>
2021-05-29 19:39:28 +0530

    adding counter

---
+++ src/Components/counter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import Reveal from 'react-reveal/Reveal';
+import CountUp from 'react-countup';
+import "./counter.css";
+function Counter(props){
+
+  const [countvis,updatecount]=React.useState(true);
+
+    return(
+<VisibilitySensor>
+      {({isVisible}) =>{
+        return(
+      <Reveal effect="animation">
+      <div className="counter-div ">
+      <div className="counter-text">
+       <CountUp
+       start={(isVisible&&countvis)? (0,updatecount(false)): -1}
+       end= {props.end}
+       duration={2.5}
+      />
+      <container><p>No of Participants</p> </container>
+    
+     </div>
+    </div>
+    </Reveal>
+        )
+      }
+      }
+    </VisibilitySensor>
+    )}
+  
+    export default Counter;
