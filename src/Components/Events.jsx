e8969023064bf855a411df4a7d28f6b299c8c775
Rudraksh <rudrakshsattabhayya@gmail.com>
2021-06-10 13:24:42 +0530

    pushing changes

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -62,7 +62,7 @@ function Event(props){
             <div class="card event-card">
             <div className="forAnimation"></div>
               <div class="card-body event-card-body dimg_css">
-              <Counter end="0" />
+              <Counter end="40" />
               <a onClick={cp} href="#" className="Nodeco">
                 <h2>Competitive Coding</h2>
                 <div class="line">
