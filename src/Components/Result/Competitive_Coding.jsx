66171c78efbb3a33c176e3c0d931a36041f5cd7f
saurabh021120 <saurabhratnaparkhi11@gmail.com>
2021-06-28 10:31:04 +0530

    cp result page

--- src/Components/Result/Competitive_Coding.jsx
+++ src/Components/Result/Competitive_Coding.jsx
@@ -35,7 +35,7 @@ function Competitive_Coding() {
             <MDBCol md="4 rankcolcss">
               <img
                 className="heightcss"
-                src="./Images/Result Images/CP/Pratik.jpeg"
+                src="./Images/Result images/CP/Pratik.jpeg"
               />
               <h1 className="rankcssh1">1st Rank</h1>
               <h5
